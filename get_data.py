d229e9d4ab7d3c1c02f51f36834b1abd92574494
Paul Russum <paul.russum@gmail.com>
2019-03-29 07:24:39 -0500

    updated setup.py and made get_data independently runable

--- get_data.py
+++ get_data.py
@@ -52,3 +52,17 @@ def get_station_data():
     session.commit()
     session.close()
 
+
+def main():
+    # 2018_Q1 uses different column names, not compatible with earlier years
+    print("getting ride data")
+    file_list = ['Divvy_Trips_2018_Q2', 'Divvy_Trips_2018_Q3', 'Divvy_Trips_2018_Q4']
+    get_ride_data(file_list=file_list, trunc=True)
+    print("getting station data")
+    get_station_data()
+
+
+if __name__ == '__main__':
+    main()
+
+
--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 
-from get_data import get_ride_data, get_station_data
+from distutils import setup
 
-
-def main():
-    # 2018_Q1 uses different column names, not compatible with earlier years
-    print("getting ride data")
-    file_list = ['Divvy_Trips_2018_Q2', 'Divvy_Trips_2018_Q3', 'Divvy_Trips_2018_Q4']
-    get_ride_data(file_list=file_list, trunc=True)
-    print("getting station data")
-    get_station_data()
-
-
-if __name__ == '__main__':
-    main()
+setup(name='Flask-Divvy',
+      version='0.1',
+      description='PageRank of bike trips to and from Divvy stations',
+      author='Paul Russum',
+      author_email='paul.russum@gmail.com',
+      packages=['app']
+      )
