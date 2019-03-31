59d23af1694feaeb16c9bb2b7a5b2197d881b24a
Paul Russum <paulr@Pauls-MacBook-Pro.local>
2019-03-30 19:26:28 -0500

    updates to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ from distutils import setup
 setup(name='Flask-Divvy',
       version='0.1',
       description='PageRank of bike trips to and from Divvy stations',
-      author='Paul Russum',
-      author_email='paul.russum@gmail.com',
       packages=['app']
       )
